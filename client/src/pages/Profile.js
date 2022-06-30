88e07dce9346d4f94a0f14ce5e8a1482964522aa
Robert D Gonzales <robert.gonzo13@yahoo.com>
2022-06-29 19:13:16 -0500

    added ADD_FRIEND mutation and added it to Profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import ThoughtList from '../components/ThoughtList';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { ADD_FRIEND } from '../utils/mutations';
 import FriendList from '../components/FriendList';
 import Auth from '../utils/auth';
 
 const Profile = () => {
+  const [addFriend] = useMutation(ADD_FRIEND);
+
   const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
@@ -14,6 +17,16 @@ const Profile = () => {
   })
 
   const user = data?.me || data?.user || {};
+
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   if (Auth.loggedIn() && Auth.getProile().data.username === userParam) {
     return <Navigate to="/profile" />
@@ -37,6 +50,12 @@ const Profile = () => {
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
+
+        {userParam && (
+        <button className="btn ml-auto" onClick={handleClick}>
+          Add Friend
+        </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
